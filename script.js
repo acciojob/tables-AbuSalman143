e366de78ac65f220c061107c0f714caac7ffa70a
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-06-13 00:38:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+function updateTotal() {
+			var total = 0;
+			var rows = document.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+			for (var i = 0; i < rows.length; i++) {
+				var amount = parseInt(rows[i].getElementsByTagName('td')[3].innerText);
+				total += amount;
+			}
+			document.getElementById('total').innerText = total;
+		}
+
+		updateTotal();
